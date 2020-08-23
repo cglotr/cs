ee80e4d65930d51dde8ee84e20f1952f072d7212
cglotr <amir.ariffin.1992@gmail.com>
2020-08-23 13:21:46 +0800

    rename to dsu

--- dsu.py
+++ dsu.py
@@ -1,8 +1,8 @@
-class UnionFind():
+class DSU():
     def __init__(self, n):
         self.parent = [i for i in range(n)]
         self.size = [1 for _ in range(n)]
-        
+
     def union(self, a, b):
         roota = self.find(a)
         rootb = self.find(b)
@@ -15,17 +15,17 @@ class UnionFind():
             self.size[roota] += self.size[rootb]
             self.parent[rootb] = roota
         return True
-    
+
     def find(self, a):
         root = self.parent[a]
         while root != self.parent[root]:
             root = self.parent[root]
         curr = a
-        
+
         # path compression
         while curr != root:
             after = self.parent[curr]
             self.parent[curr] = root
             curr = after
-        
+
         return root
