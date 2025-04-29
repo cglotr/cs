13bdeaf53da30c5d2f35ea7162d90d2fd4facaef
Amir Ariffin <22372978+cglotr@users.noreply.github.com>
2025-04-28 19:17:01 -0700

    Create sln_two_pointer.py

---
+++ leetcode/000033/sln_two_pointer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            m = l + (r - l)//2
+
+            if nums[m] == target:
+                return m
+
+            if nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+        return -1
