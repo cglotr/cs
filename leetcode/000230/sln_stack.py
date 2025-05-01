4fa90f2965d055ce3f75c24e47ea4ba14f2e5b05
Amir Ariffin <22372978+cglotr@users.noreply.github.com>
2025-04-30 22:49:58 -0700

    Create sln_stack.py

---
+++ leetcode/000230/sln_stack.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        cur = root
+        stack = []
+        count = k
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            count -= 1
+            if count <= 0:
+                return cur.val
+
+            cur = cur.right
+
+        return None
